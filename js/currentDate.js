17586f1a54cf064cf42eb41d49fe6c8597f9a7e6
Valkkyri <melissa.deshi@gmail.com>
2020-09-18 18:45:16 -0600

    Assignmet Portal w/fancy JS

--- js/currentDate.js
+++ js/currentDate.js
@@ -1,39 +1,68 @@
-// toLocaleDateString
-/*
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
-*/
+function currentDate() {
+    let dayNames = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ]
+    let monthNames = [
+        'January',
+        'Febuary',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ]
 
-const dayNames = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday'
-]
+    let today = new Date();
+    let todayName = dayNames[today.getDay()];
+    let thisMonth = monthNames[today.getMonth()];
+    let currentDate = todayName + ', ' + thisMonth + ' ' + today.getDate() + ' ' + today.getFullYear();
+    let time = currentHour();
 
-const monthNames = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sept',
-    'Oct',
-    'Nov',
-    'Dec'
-]
-const today = new Date();
-const todayName = dayNames[today.getDay()];
-const thisMonth = monthNames[today.getMonth()];
+    document.getElementById('lastUpdated').textContent = currentDate + time;
+    document.getElementById('currentYear').textContent = today.getFullYear();
+}
 
-const currentDate = todayName + ', ' + thisMonth + ' ' + today.getDate() + ' ' + today.getFullYear();
+function currentHour() {
+    let today = new Date();
+    let hour = today.getHours();
+    let mins = currentMins();
+    
+    if (hour == 0) {
+        return ' at ' + 12 + mins + ' AM';
+    }
+    else if (hour == 12) {
+        return ' at ' + 12 + mins + ' PM';
+    }
+    else if (hour < 24 && hour > 12) {
+        let pmHour = hour - 12;
+        return ' at ' + pmHour + mins + ' PM';
+    }
+    else {
+        let amHour = hour;
+        return ' at ' + amHour + mins + ' AM';
+    }    
+}
 
-
-document.getElementById('currentDate').textContent = currentDate;
-
+function currentMins() {
+    let today = new Date();
+    let mins = today.getMinutes();
+    if (mins < 10) {
+        let smallMins = ':0' + mins;
+        return smallMins;
+    }
+    else {
+        return ':' + mins;
+    }
+    
+}
