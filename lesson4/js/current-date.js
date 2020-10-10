dc880030f416ce73629b55da72b12e23b4ca8470
Valkkyri <melissa.deshi@gmail.com>
2020-10-10 16:43:48 -0600

    today's date JS

---
+++ lesson4/js/current-date.js
@@ -0,0 +1,69 @@
+window.onload = currentDate();
+
+function currentDate() {
+    let dayNames = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ]
+    let monthNames = [
+        'January',
+        'Febuary',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ]
+
+    let today = new Date();
+    let todayName = dayNames[today.getDay()];
+    let thisMonth = monthNames[today.getMonth()];
+    let currentDate = todayName + ', ' + thisMonth + ' ' + today.getDate() + ' ' + today.getFullYear();
+    let time = currentHour();
+
+    document.getElementById('todaysDate').innerHTML = currentDate + time;
+}
+
+function currentHour() {
+    let today = new Date();
+    let hour = today.getHours();
+    let mins = currentMins();
+    
+    if (hour == 0) {
+        return ', ' + 12 + mins + ' AM';
+    }
+    else if (hour == 12) {
+        return ', ' + 12 + mins + ' PM';
+    }
+    else if (hour < 24 && hour > 12) {
+        let pmHour = hour - 12;
+        return ', ' + pmHour + mins + ' PM';
+    }
+    else {
+        let amHour = hour;
+        return ', ' + amHour + mins + ' AM';
+    }    
+}
+
+function currentMins() {
+    let today = new Date();
+    let mins = today.getMinutes();
+    if (mins < 10) {
+        let smallMins = ':0' + mins;
+        return smallMins;
+    }
+    else {
+        return ':' + mins;
+    }
+    
+}
