8836fdbd113f7ded0506f1395fb500f4b86f076d
Valkkyri <melissa.deshi@gmail.com>
2020-10-28 15:36:44 -0600

    gallery

---
+++ lesson7/js/windchill.js
@@ -0,0 +1,15 @@
+window.onload = calculateWindchill();
+
+function calculateWindchill() {
+    let highTemp = parseFloat(document.querySelector("#high-temp").innerText);
+    let windSpeed = parseFloat(document.querySelector("#wind-speed").innerText);
+    let windChill = document.querySelector("#wind-chill").innerText = "N/A";
+
+    if (highTemp <= 50 && windSpeed > 3) {
+        windChill = 35.74 + (0.6215 * highTemp) - (35.75 * windSpeed ** 0.16) + (0.4275 * highTemp * windSpeed ** 0.16);
+        document.querySelector("#wind-chill").innerHTML = Math.round(windChill.toFixed(2)) + "&deg;F";
+    }
+    else {
+        document.querySelector("#wind-chill").innerText = windChill;
+    }
+}
