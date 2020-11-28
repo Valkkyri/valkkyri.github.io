847aea31a85f25c80a19fb3b1cd7f99e569deeb7
Valkkyri <melissa.deshi@gmail.com>
2020-11-28 15:19:55 -0700

    script updated to use both weather and forcast API

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,25 +1,17 @@
 // Preston Townpage Code
-const prestonAPIurl = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=91a14b3e44979104098259be0b0f8f5c&units=imperial";
+const prestonForcast = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=91a14b3e44979104098259be0b0f8f5c&units=imperial";
+const prestonCurrent = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=91a14b3e44979104098259be0b0f8f5c&units=imperial";
 
-fetch(prestonAPIurl)
+//Current Weather
+fetch(prestonCurrent)
     .then((response) => response.json())
-    .then((jsObject) => {
-        /* ------ current weather start ------ */
+    .then((jsObject) => {        
         //console.log(jsObject);
-        let currentConditions = jsObject.list[0].weather[0].main;
-        let temp = jsObject.list[0].main.temp.toFixed(1);
-        let windSpeed = jsObject.list[0].wind.speed.toFixed(0);
-        let humidity = jsObject.list[0].main.humidity;
-        let windChill = "N/A";
-        
-        // Calculate Wind Chill
-        if (temp <= 50 && windSpeed > 3) {
-            windChill = 35.74 + (0.6215 * temp) - (35.75 * windSpeed ** 0.16) + (0.4275 * temp * windSpeed ** 0.16);
-            windChill = `${Math.round(windChill.toFixed(2))} &deg;F`;
-        }
-        else {
-            windChill = "N/A";
-        }
+        let currentConditions = jsObject.weather[0].main;
+        let temp = jsObject.main.temp.toFixed(1);
+        let windSpeed = jsObject.wind.speed.toFixed(0);
+        let humidity = jsObject.main.humidity;
+        let windChill = calculateWindChill(temp, windSpeed);
 
         let summaryUL = document.createElement("ul");
         let summarydetails = [`${currentConditions}`, `<span id="high-temp">${temp}</span>&deg;F`, `${windChill}`, `${humidity}%`, `<span id="wind-speed">${windSpeed}</span> mph`];
@@ -29,13 +21,16 @@ fetch(prestonAPIurl)
             summaryUL.appendChild(li);
         });
         
-        document.querySelector("section.stats-right").appendChild(summaryUL);
-        /* ------ current weather end ------ */
+        document.querySelector("section.stats-right").appendChild(summaryUL);  
+    });
 
-        
-        /* ------ five day forecast start ------ */
+// Five-day Forecast
+fetch(prestonForcast)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        //console.log(jsObject);
         let forcast = jsObject.list.filter(i => i.dt_txt.includes("18:00:00"));
-        console.log(forcast);
+        //console.log(forcast);
         let weekdays = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
         let forcastUL = document.createElement("ul");
 
@@ -50,5 +45,17 @@ fetch(prestonAPIurl)
             });
         
         document.querySelector("section.forcast").appendChild(forcastUL);
-        /* ------ five day forecast end ------ */    
-    });  
+    });
+
+// Calculate Wind Chill
+function calculateWindChill(temp, windSpeed) {
+    if (temp <= 50 && windSpeed > 3) {
+        windChill = 35.74 + (0.6215 * temp) - (35.75 * windSpeed ** 0.16) + (0.4275 * temp * windSpeed ** 0.16);
+        windChill = `${Math.round(windChill.toFixed(2))}&deg;F`;
+        return windChill;
+    }
+    else {
+        windChill = "N/A";
+        return windChill;
+    }
+};
